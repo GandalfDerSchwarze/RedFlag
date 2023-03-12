2a703f0d3ced0880b61d364855d193ec7e63a2ae
Jan Margreiter <jan.margreiter@tum.de>
2023-03-12 15:35:27 +0100

    hello world javafx should be working now

--- redflag-gui-javafx/src/main/java/module-info.java
+++ redflag-gui-javafx/src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.example.redflagguijavafx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+        requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            //requires validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            //requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
 
     opens com.example.redflagguijavafx to javafx.fxml;
     exports com.example.redflagguijavafx;
