8f506f2869ae96d0ac8ec0e181df3ecb4431141b
Draggon1 <leonunterberger@gmx.at>
2023-03-20 21:25:52 +0100

    RF00 - startet spring boot impl

--- Redflag-backend/src/main/java/com/backend/redflagbackend/rest/testRest.java
+++ Redflag-backend/src/main/java/com/backend/redflagbackend/rest/testRest.java
@@ -3,13 +3,11 @@ package com.backend.redflagbackend.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class testRest {
 
     @GetMapping("test")
-    public List<String> test() {
-        return List.of("a", "asdoc");
+    public String test() {
+        return "hey";
     }
 }
