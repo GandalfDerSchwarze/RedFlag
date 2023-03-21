2ee791d586a9f8271cabb6ad16a0f5e489f1f540
Draggon1 <leonunterberger@gmx.at>
2023-03-21 22:34:41 +0100

    RF00 - played arround with webpack and so on

--- Redflag-backend/src/main/java/com/backend/redflagbackend/rest/testRest.java
+++ Redflag-backend/src/main/java/com/backend/redflagbackend/rest/testRest.java
@@ -3,11 +3,13 @@ package com.backend.redflagbackend.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class testRest {
 
     @GetMapping("test")
-    public String test() {
-        return "hey";
+    public List<String> test() {
+        return List.of("a", "asdoc");
     }
 }
