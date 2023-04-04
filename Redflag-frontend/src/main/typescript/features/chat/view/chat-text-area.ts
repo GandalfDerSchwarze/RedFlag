7c435a8a963f4a1463b6e413f3fe1191dbeb6a38
Draggon1 <leonunterberger@gmx.at>
2023-04-05 00:07:35 +0200

    RF00 - image example

--- Redflag-frontend/src/main/typescript/features/chat/view/chat-text-area.ts
+++ Redflag-frontend/src/main/typescript/features/chat/view/chat-text-area.ts
@@ -12,15 +12,21 @@ export class ChatTextArea extends LitElement {
                 height: 100%;
                 width: 100%;
             }
+
+            .testImage {
+                background: url('/static/assets/background-main-layout-new.jpg');
+            }
+            
         `;
     }
 
     render() {
         // language=html
         return html`
-            <div style="width: 100%; height: 100%;">
-
+            <div class="testImage" style="width: 100%; height: 100%;">
             </div>
+
+            
             <img src="/static/assets/background-main-layout-new.jpg">
         `;
     }
